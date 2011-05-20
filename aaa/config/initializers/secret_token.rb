984f4734871527faac8322879b523216386136cc
rakesh1 <rakesh@futurenow.biz>
2011-05-20 17:38:05 +0530

    intial commit

---
+++ aaa/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aaa::Application.config.secret_token = '0d1669763a6a3b5540d92eeb137469abd6e644f448dd5da5ff59421b5712a11bd6dee866d755cde358a3edc2920fa41ea952365afe4e86723d7bf369721380b9'
